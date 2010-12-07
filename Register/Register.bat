f62b98b95cba6d88645e270d5a77ee8444bd6086
masamunexgp <masamunexgp@github.com>
2010-12-07 08:14:50 +0000

    Tightened up Register project.

--- Register/Register.bat
+++ Register/Register.bat
@@ -1,9 +1,13 @@
 echo off
 
+call VCVarsQueryRegistry.bat 32bit 64bit
 cd ..\QTTabBar\bin\%1
 IF EXIST QTTabBar.dll (
     gacutil /if QTTabBar.dll
-    regasm QTTabBar.dll
+    call %FrameworkDir32%\%FrameworkVersion32%\regasm.exe QTTabBar.dll
+    if not "%FrameworkDir64%"=="" (
+        call %FrameworkDir64%\%FrameworkVersion64%\regasm.exe QTTabBar.dll
+    )
 )
 
 cd ..\..\..\QTPluginLib\bin\%1
