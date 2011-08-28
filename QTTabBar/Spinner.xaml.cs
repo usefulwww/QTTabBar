1173b30fa10de6d36c8d311ad9264fd1911fbf21
jeromedoby <jeromedoby@github.com>
2011-08-28 07:29:32 +0000

    * Fixed data binding issue for the Spinner * Removed the need for mouse events to trigger saving the value of the scroll bar.

--- QTTabBar/Spinner.xaml.cs
+++ QTTabBar/Spinner.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -18,11 +19,14 @@ namespace QTTabBarLib {
     /// </summary>
     public partial class Spinner : UserControl {
         private int _value = 0;
+        private bool _doRecurse = false;
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register( "Value", typeof (int), typeof(Spinner), new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault) );
+
         public int Value {
-            get { return (int)scrollBar.Value; }
-            set { 
-                scrollBar.Value = value;
-                txtValue.Text = value.ToString();
+            get { return (int)GetValue(ValueProperty); }
+            set {
+                SetValue( ValueProperty, value );
             }
         }
 
@@ -32,41 +36,55 @@ namespace QTTabBarLib {
 
         //# Make sure only numbers are entered.
         private void txtValue_TextChanged(object sender, RoutedEventArgs e) {
-            if(!int.TryParse(txtValue.Text, out _value)) {
-                txtValue.Text = scrollBar.Value.ToString();
+            if(!_doRecurse) {
+                _doRecurse = true;
                 return;
             }
 
-            //# Why does this crash Explorer? This should not recurse...
+            if(!int.TryParse(txtValue.Text, out _value)) {
+                txtValue.Text = Value.ToString();
+                return;
+            }
 
-            /*if(int.Parse( txtValue.Text ) > scrollBar.Maximum) {
+            _doRecurse = false;
+            
+            if(int.Parse( txtValue.Text ) > scrollBar.Maximum) {
                 txtValue.Text = scrollBar.Maximum.ToString();
             }else if(int.Parse(txtValue.Text) < scrollBar.Minimum) {
                 txtValue.Text = scrollBar.Minimum.ToString();
-            }*/
+            }
+
+            Value = int.Parse(txtValue.Text);
         }
 
         //# Increment, or decrement on up, or down arrow key usage.
-        private void txtValue_KeyUp(object sender, KeyEventArgs e) {
+        private void txtValue_KeyDown(object sender, KeyEventArgs e) {
            if(e.Key == Key.Up) {
-               scrollBar.Value++;
+               Value++;
            }
 
            if(e.Key == Key.Down) {
-               scrollBar.Value--;
+               Value--;
            }
         }
 
         //# Ensure the txtValue field always reflects the value of the ScrollBar
         private void scrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
-            txtValue.Text = scrollBar.Value.ToString();
-        }
-
-        //# Make sure that when the mouse leaves the txtValue field, that the value inside the TextBox is stored as the ScrollBar value
-        private void txtValue_MouseLeave(object sender, MouseEventArgs e) {
-            Value = int.Parse( txtValue.Text );
+            if(!_doRecurse) {
+                _doRecurse = true;
+            }
+            txtValue.Text = Value.ToString();
         }
     }
 
+    [ValueConversion(typeof(int), typeof(double))]
+    public class DoubleToIntegerConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return (double)(int)value;
+        }
 
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            return (int)(double)value;
+        }
+    }
 }
