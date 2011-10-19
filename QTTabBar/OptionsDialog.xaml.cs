04f56f9f8608149b08d1459f00c559069e3c5d20
kawanon <kawanon@github.com>
2011-10-19 10:32:17 +0000

    Added TreeListView and its resources.

--- QTTabBar/OptionsDialog.xaml.cs
+++ QTTabBar/OptionsDialog.xaml.cs
@@ -1458,7 +1458,7 @@ namespace QTTabBarLib {
 
         #endregion
 
-        #region ---------- ListBox Item Classes ----------
+        #region ---------- Binding Classes ----------
 
         private class ButtonEntry {
             private OptionsDialog parent;
@@ -1802,6 +1802,24 @@ namespace QTTabBarLib {
         }
     }
 
+    /// <summary>
+    /// Convert Level to left margin
+    /// Pass a prarameter if you want a unit length other than 19.0.
+    /// </summary>
+    public class LevelToIndentConverter : IValueConverter {
+        public object Convert(object o, Type type, object parameter,
+                              CultureInfo culture) {
+            return new Thickness((int)o * c_IndentSize, 0, 0, 0);
+        }
+
+        public object ConvertBack(object o, Type type, object parameter,
+                                  CultureInfo culture) {
+            throw new NotSupportedException();
+        }
+
+        private const double c_IndentSize = 19.0;
+    }
+
     #endregion
 
     // Overloaded RadioButton class to work around .NET 3.5's horribly HORRIBLY
@@ -1843,4 +1861,49 @@ namespace QTTabBarLib {
             rbx.bIsChanging = false;
         }
     }
+
+    #region ---------- TreeListView ----------
+
+    public class TreeListView : TreeView {
+        protected override DependencyObject
+                           GetContainerForItemOverride() {
+            return new TreeListViewItem();
+        }
+
+        protected override bool
+                           IsItemItsOwnContainerOverride(object item) {
+            return item is TreeListViewItem;
+        }
+    }
+
+    public class TreeListViewItem : TreeViewItem {
+        /// <summary>
+        /// Item's hierarchy in the tree
+        /// </summary>
+        public int Level {
+            get {
+                if(_level == -1) {
+                    TreeListViewItem parent =
+                        ItemsControl.ItemsControlFromItemContainer(this)
+                            as TreeListViewItem;
+                    _level = (parent != null) ? parent.Level + 1 : 0;
+                }
+                return _level;
+            }
+        }
+
+
+        protected override DependencyObject
+                           GetContainerForItemOverride() {
+            return new TreeListViewItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item) {
+            return item is TreeListViewItem;
+        }
+
+        private int _level = -1;
+    }
+
+    #endregion
 }
