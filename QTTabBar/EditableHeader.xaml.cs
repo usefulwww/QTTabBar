40ba124e02269fcb591239a10dde6d19fbc0e873
kawanon <kawanon@github.com>
2011-10-22 03:57:51 +0000

    Fixed issue where margins of Group folders are different.  Exposed some properties and methods of EditableHeader.

--- QTTabBar/EditableHeader.xaml.cs
+++ QTTabBar/EditableHeader.xaml.cs
@@ -20,6 +20,7 @@ namespace QTTabBarLib
     public partial class EditableHeader : UserControl {
 
         private bool preparing = false;
+        private string originalText;
 
         public string Text {
             get { return (string)GetValue(TextProperty); }
@@ -31,12 +32,31 @@ namespace QTTabBarLib
             set { SetValue(ContainerProperty, value); }
         }
 
+        public bool ReadOnly {
+            get { return (bool)GetValue(ReadOnlyProperty); }
+            set { SetValue(ReadOnlyProperty, value); }
+        }
+
+        public bool CancelIfEmpty {
+            get { return (bool)GetValue(CancelIfEmptyProperty); }
+            set { SetValue(CancelIfEmptyProperty, value); }
+        }
+
         public static readonly DependencyProperty TextProperty =
             DependencyProperty.Register("Text", typeof(string), typeof(EditableHeader),
             new FrameworkPropertyMetadata("New Header", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
         public static readonly DependencyProperty ContainerProperty =
             DependencyProperty.Register("Container", typeof(UIElement), typeof(EditableHeader),
             new FrameworkPropertyMetadata(null));
+
+        public static readonly DependencyProperty ReadOnlyProperty =
+            DependencyProperty.Register("ReadOnly", typeof(bool), typeof(EditableHeader),
+            new FrameworkPropertyMetadata(false));
+
+        public static readonly DependencyProperty CancelIfEmptyProperty =
+            DependencyProperty.Register("CancelIfEmpty", typeof(bool), typeof(EditableHeader),
+            new FrameworkPropertyMetadata(true));
 
         public EditableHeader(string initialName, TreeViewItem item = null) {
             InitializeComponent();
@@ -51,33 +71,52 @@ namespace QTTabBarLib
         public EditableHeader() : this(null) {
         }
 
-        private void txtHeaderEdit_LostFocus(object sender, RoutedEventArgs e) {
-            //txtHeader.Text = txtHeaderEdit.Text;
+        public void EndEdit() {
             txtHeaderEdit.Visibility = Visibility.Hidden;
+
+            BindingExpression expr = txtHeaderEdit.GetBindingExpression(TextBox.TextProperty);
+            expr.UpdateSource();
+
+            if(CancelIfEmpty && string.IsNullOrEmpty(Text)) {
+                Text = originalText;
+            }
+        }
+
+        public void StartEdit() {
+            originalText = Text;
+            txtHeaderEdit.Visibility = Visibility.Visible;
+            txtHeaderEdit.CaptureMouse();
+            txtHeaderEdit.Focus();
+            txtHeaderEdit.ReleaseMouseCapture();
+        }
+
+        private void txtHeaderEdit_LostFocus(object sender, RoutedEventArgs e) {
+            EndEdit();
         }
 
         private void txtHeaderEdit_KeyDown(object sender, KeyEventArgs e) {
             if(e.Key == Key.Enter) {
-                //txtHeader.Text = txtHeaderEdit.Text;
-                txtHeaderEdit.Visibility = Visibility.Hidden;
+                EndEdit();
             }
         }
 
         private void txtHeader_MouseDown(object sender, MouseButtonEventArgs e) {
+            if(ReadOnly) {
+                // We can add a sound if you need to do so.
+                return;
+            }
             preparing = (Container != null)
                 ? Container.IsFocused
                 : true;
         }
 
         private void txtHeader_MouseUp(object sender, MouseButtonEventArgs e) {
-            // Does not open the text box if editable area has got focus by clicking right now.
+            // Does not open the text box if the editable area has got focus by clicking right now.
             if(!preparing) {
                 return;
             }
-            txtHeaderEdit.Visibility = Visibility.Visible;
-            txtHeaderEdit.CaptureMouse();
-            txtHeaderEdit.Focus();
-            txtHeaderEdit.ReleaseMouseCapture();
+
+            StartEdit();
         }
     }
 }
